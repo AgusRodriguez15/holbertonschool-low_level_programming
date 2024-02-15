99cd208818f3ab96ef2002f4fbd1dd8a22f7c02a
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-15 06:15:09 -0800

    pepe

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+	 
+
+{
+	printf("Size of a int:%lu\n",sizeof(char));
+	printf("Size of a int:%lu\n",sizeof(int));
+	printf("Size of a int:%lu\n",sizeof(long int));
+	printf("Size of a int:%lu\n",sizeof(long long int));
+	printf("Size of a int:%lu\n",sizeof(float));
+
+	return (0);
+}	
+	
