b07e2edc5dc0ccca26babeaa00fb622b83b70cee
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-15 10:03:36 -0800

    pepe

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	   if(n>0) {
+	   if(n > 0) 
+	   {
                 printf("is positive");
         }
-        else {
-                printf("is negative");
-        }
-        if (n==0){
+        else if (n ==0) 
+	{
                 printf("is zero");
+        }
+	   else (n < 0);
+	{
+                printf("is negative");
         }
         
 	return (0);
