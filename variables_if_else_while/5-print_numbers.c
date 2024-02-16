9644ef247eb7c34546b6d8d525e857491d740b50
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-16 08:23:40 -0800

    pepe

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+        int n=0;
+
+        for (n = 0; n <= 9; n++)
+		
+        {
+        printf("%d",n);
+
+        }
+printf("\n");
+        return (0);
+}
