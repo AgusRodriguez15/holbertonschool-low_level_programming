0fb81e0a52df14aab1815ae2316c5c99085c7bef
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-16 08:41:55 -0800

    pepe

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+        int n = 48;
+	int a = 57;
+
+        for (n = 48; n <= 57; n++)
+        {
+        putchar(n);
+        }
+        putchar('\n');
+        return (0);
+}
