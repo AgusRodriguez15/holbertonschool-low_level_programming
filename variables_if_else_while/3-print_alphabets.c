84a4fe950a766dabf25bea3f8d219b19a243f867
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-16 06:21:36 -0800

    pepe

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,18 @@
 int main(void)
 {
 
-          char a;
+	char a;
 
-        for (a = 'a'; a <= 'z'; a++)
-        {
-        putchar(a);
+	for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
 
-       }
-        
+}
 
-        for (a = 'A'; a <= 'Z'; a++)
-        {
-        putchar(a);
-
-        }
+	for (a = 'A'; a <= 'Z'; a++)
+{
+	putchar(a);
+}
 putchar('\n');
-        return (0);
+	return (0);
 }
