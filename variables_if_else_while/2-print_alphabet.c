48347c4e6c99ec7928a2579fceeedc8290738637
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-16 05:44:43 -0800

    pepe

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char a = 97;
+	char z = 122;
+
+	for (a = 97; a <= z; a++)
+	{ 
+		putchar(a);
+		
+	}
+        putchar('\n');
+	return(0);
+}	
