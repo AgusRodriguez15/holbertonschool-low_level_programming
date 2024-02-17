9e44191607ecdd88bd4f5b9b9b11f49c84441f65
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-17 12:38:34 -0800

    pepe

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+       int a = 97;
+       
+
+        while ( a < 123) {
+        
+        putchar(a);
+	
+	a++;
+        
+	}
+	putchar('\n');
+	 return (0);
+        
+}
