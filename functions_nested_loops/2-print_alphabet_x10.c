8e8bfef7abf38611da4fe96208958af0d1632666
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-18 06:45:34 -0800

    pepe

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - print agus
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+{
+
+        char a = 97;
+	int j;
+
+        while (a <= 122)
+        { 
+        _putchar(a);
+        a++;
+
+        }
+	for (j = 0; j < 10; j++)
+	{
+		  _putchar(a);
+	}
+        _putchar('\n');
+
+}
