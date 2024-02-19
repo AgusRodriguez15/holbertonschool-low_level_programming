d147b918b15efa62c5c21829bd3fccc3ae77f6bb
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-19 15:46:25 -0800

    file8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,3 +1,13 @@
 #include "main.h"
 
+void jack_bauer(void)
+{
+	int H;
+	int M;
 
+	for (H = 0; H <= 23; H++)
+	       for (M = 0; M <= 59; M++)
+		       _putchar(H);
+	_putchar(M);
+	_putchar('\n');
+}
