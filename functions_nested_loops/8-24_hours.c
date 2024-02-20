123568c78897c93804f58de0f64593bd6e339981
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-19 16:43:13 -0800

    file8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,10 +6,15 @@ void jack_bauer(void)
 	int M;
 
 	for (H = 0; H <= 23; H++)
+	{
 	       for (M = 0; M <= 59; M++)
-		       _putchar('0' + H / 10);
+	       {
+	_putchar('0' + H / 10);
 	_putchar('0' + H % 10 );
-	 _putchar('0' + M / 10);
-	  _putchar('0' + M % 10 );
+	_putchar(':');
+	_putchar('0' + M / 10);
+	_putchar('0' + M % 10 );
 	_putchar('\n');
+	       }
+	}
 }
