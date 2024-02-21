8df9d480ac18c80c83535389add846ba47cab74e
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-21 02:23:54 +0000

    proFour

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0.
+ */
 void jack_bauer(void)
 {
 	int H;
 	int M;
 
-	for (H = 0; H <= 23; H++)
+for (H = 0; H <= 23; H++)
 	{
-	       for (M = 0; M <= 59; M++)
-	       {
+for (M = 0; M <= 59; M++)
+	{
 	_putchar('0' + H / 10);
-	_putchar('0' + H % 10 );
+	_putchar('0' + H % 10);
 	_putchar(':');
 	_putchar('0' + M / 10);
-	_putchar('0' + M % 10 );
+	_putchar('0' + M % 10);
 	_putchar('\n');
-	       }
+	}
 	}
 }
