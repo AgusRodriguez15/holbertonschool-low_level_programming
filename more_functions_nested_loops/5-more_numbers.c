7ade1073b852d5e015140036e49909fbd9253c02
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-21 01:44:41 +0000

    proFour

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,12 @@ void more_numbers(void)
 
 	for (; b < 10; b++)
 	{
-		for (a = 0; a < 15; a++)
+	for (a = 0; a < 15; a++)
 		{
-			if (a > 9)
-				_putchar('0' + a / 10);  
-			
-			_putchar('0' + a % 10);
-		}
-	
-			
+	if (a > 9)
+	_putchar('0' + a / 10);
+	_putchar('0' + a % 10);
+	}
 	_putchar('\n');
 	}
-
-}	
+}
