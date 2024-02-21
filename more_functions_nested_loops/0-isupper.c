93a39e9e77b4adb39eb073726464d1e00ce782f7
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-21 01:53:57 +0000

    proFour

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper -Returns 1 if c is uppercase, Returns 0 otherwise
+ * _isupper -Returns 1 if int c is uppercase, Returns 0 otherwise
  *
  * Return: Always 0.
  */
