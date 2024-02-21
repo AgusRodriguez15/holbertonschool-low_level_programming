7f20413987800e71a62211a1edcf7fb45ceacc3e
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-21 02:02:15 +0000

    proFour

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * mul - return the result of a multiplie of two intergers
+ *
+ * Return: Always 0.
+ */
+
 int mul(int a, int b)
 
 {
 	int c;
+
 	c = a * b;
 	return (c);
 }
