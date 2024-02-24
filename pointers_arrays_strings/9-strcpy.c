2097eb656d6468a90499da3999ded741774ae923
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-24 18:29:55 +0000

    pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src
+ *@src : char
+ *@dest : char
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *a;
+	
+	while ((*dest++ = *src++) != '\0')
+	{
+a = dest;
+	}
+	return (a);
+}
