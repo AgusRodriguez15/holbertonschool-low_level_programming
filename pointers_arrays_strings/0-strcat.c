a40cc3ed1cf98d9b974ea602776e530ddb24be02
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-25 16:52:09 +0000

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 0-strcat.c - concatenetes the string pointed to by src
+ * *_strcat - concatenetes the string pointed to by src
+ *_strlen - calculates the length of dest
  *@src : char
  *@dest : char
  *
  * Return: Always 0.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 int i = 0;
 int b;
-        while (src[i] != '\0')
-        i++;
-        for (b = 0; b < i; b++)
-        {
-                dest[b] += src[b];
-        }
-        dest[b++] = '\0';
-        return (dest);
+size_t c =  _strlen(dest);
 
+	while (src[i] != '\0')
+	i++;
+	for (b = 0; b < i; b++)
+	{
+	dest[c + b] += src[b];
+	}
+	dest[c + b] = '\0';
+	return (dest);
+
+}
+
+int _strlen(char *dest)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
