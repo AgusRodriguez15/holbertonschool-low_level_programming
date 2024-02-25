df6f2027da56240e0a4a719285c1486181571017
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-25 15:58:09 +0000

    pointers

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * 0-strcat.c - concatenetes the string pointed to by src
+ *@src : char
+ *@dest : char
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+int b;
+        while (src[i] != '\0')
+        i++;
+        for (b = 0; b < i; b++)
+        {
+                dest[b] += src[b];
+        }
+        dest[b++] = '\0';
+        return (dest);
+
+}
