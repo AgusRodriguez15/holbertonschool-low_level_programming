724287a67d8699af16a0034ea2aea7d8865c075f
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-26 13:31:14 +0000

    pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * rev_string - print the reverse of a string 
+ * rev_string - print the reverse of a string
  * @s : char
  *
  * Return: Always 0.
  */
-
 void rev_string(char *s)
-
 {
-int i = 0, b;
-char c;
+	int i = 0, b;
+	 char c;
 
-while (s[i] != '\0')
-i++;
-i--;
+	while (s[i] != '\0')
+	i++;
+	i--;
 
-for (b = 0; b <= i / 2; b++)
-{
-c = s[b];
-s[b] = s[i - b];
-s[i - b] = b;
-
+	for (b = 0; b <= i / 2; b++)
+	{
+	c = s[b];
+	s[b] = s[i - b];
+	s[i - b] = c;
 }
 }
+
