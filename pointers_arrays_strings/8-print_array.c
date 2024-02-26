5ba8fd0f6697c738c1b1b47bbee8d67fc7797f6b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-26 12:57:39 +0000

    pointers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code for
+ * print_array - print numbers
+ *@n : int
+ *@a : int
  *
  * Return: Always 0.
  */
@@ -13,10 +15,9 @@ void print_array(int *a, int n)
 	{
 		printf("%d", a[i]);
 
-		if (i < n -1)
+		if (i < n - 1)
 		{
 			printf(", ");
-		
 
 		}
 	}
