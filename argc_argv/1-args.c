b7a0e448202efd568efe4c0730d169e607bc4936
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-28 14:44:12 +0000

    malloc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a program that prints its name, followed by a new line
+ *@argc : char
+ *@argv : char
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
