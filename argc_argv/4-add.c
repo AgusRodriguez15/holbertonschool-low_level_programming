58866f2cad84b2b97cbd69ee02a01d2e0fab97e1
AgusRodriguez15 <8670@holbertonstudents.com>
2024-02-29 17:13:54 +0000

    pointers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Write a program that prints its name, followed by a new line
+ *@argc : char
+ *@argv : char
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-  int i;
-  int suma;
-    if (argc == 1)
-    {
-      printf("0\n");
-    }
-    else
-      {
-    for (i = 1; i < argc; i++)
-	    {
-          suma += atoi(argv[i]);
-	    }
-       printf("%d\n", suma);
-      }
-  return (0);
+	int i;
+	int suma;
+
+	if (argc == 1)
+	{
+	printf("0\n");
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+	suma += atoi(argv[i]);
+	}
+	printf("%d\n", suma);
+	}
+	return (0);
 }
