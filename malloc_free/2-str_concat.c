6a8e13f03aad9bf0c9d99a080118d481ef77fe91
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-05 14:53:47 +0000

    pointers

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * _strlen - check
+ *@i:int
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *dest)
+{
+        int i = 0;
+
+        while (dest[i] != '\0')
+        {
+        i++;
+        }
+        return (i);
+}
+
+/**
+ * str_concat - check
+ *@i:int                                                                                                             *                                                                                                                   * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int lar1 = _strlen(s1);
+	int lar2 = _strlen(s2);
+	char *s3;
+	int i = 0;
+	
+	if(s1 == NULL)
+	{
+		s1 = "";
+	}
+	if(s2 == NULL)
+	{
+		s2 = "";
+	}
+	s3 = malloc(sizeof(char) * _strlen(s1) + _strlen(s2) + 1 );
+	if(s3 == NULL)
+	{
+		return (NULL);
+	}
+	for(i = 0; i < lar1; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for(i = 0; i < lar2; i++)
+	{
+		s3[lar1 + i] = s2[i];
+	}
+	s3[lar1 + lar2] = '\0';
+	return (s3);
+}
+
+
+
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,3 +1,6 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strle(char *copy);
+char *str_concat(char *s1, char *s2);
+#include <stdlib.h>
+
