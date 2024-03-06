e0c0b71183a66cc92635aa28afd48844d23be9e3
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-06 17:37:22 +0000

    pointers

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,29 +13,35 @@ int **alloc_grid(int width, int height)
 	int j = 0;
 	int i = 0;
 
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
 	p = malloc(height * sizeof(int*));
 	if (p == NULL)
-	{
 		return(NULL);
-	}
+
 	for (i = 0; i < height; i++)
 	{
-	p[i] = malloc(width * sizeof(int));
-	if (p[i] == NULL)
-	{
-	return(NULL);
-	for (; i >= 0; i--)
-	free(p[i]);
-	free(p);
-	return(NULL);
-	}	
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return(NULL);
+		}	
 
-}
+	}
+
 	for (j = 0; j < width; j++)
-{
-	 for (i = 0; i < height; i++)
-	p[i][j] = 0;
-}
+		for (i = 0; i < height; i++)
+			p[i][j] = 0;
+
 	return (p);
 }
 	
