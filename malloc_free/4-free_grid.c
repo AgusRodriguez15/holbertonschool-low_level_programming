94166e6e49b2ebb79c4fab98d711944faa2faa29
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-06 18:03:02 +0000

    pointers

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free (grid[i]);
+	}
+	free(grid);
+}
