b617beae107ced7378bb9f345f3f3ddd01744269
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-07 14:08:20 +0000

    pointers

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - check the code
+ *@b:unsigned int
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b * sizeof(unsigned int));
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
