e942d6c8796fb7019d298f38977273cab7522e0e
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-08 13:44:41 +0000

    pointers

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,13 +16,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0)
 		return (NULL);
 	a = malloc(size * nmemb);
-	{
+	
 	if (a == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = '\0';
 	}
-	  for (i = 0; i < nmemb; i++)
-	  {
-		a[i] = 0;
-	  }
-	return (a);
+	return a;
 }
