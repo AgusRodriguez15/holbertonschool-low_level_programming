115b2670bac4799c48a7d505010c21c89473d904
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-08 18:29:56 +0000

    pointers

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - check the code
+ * @name: char
+ * @owner: char
+ * @age: char
+ *
+ * Return: Always 0.
+ */
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+#endif
