4a93ed2b491d617d4154a4f1cf3291ec82837c97
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-10 23:42:32 +0000

    more_malloc

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - check the code
+ *@d: dog
+ *@name: char
+ *@age: float
+ *@owner: char
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
