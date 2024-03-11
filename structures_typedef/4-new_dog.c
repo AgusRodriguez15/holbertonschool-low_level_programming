457afa5a5c95bb66e493dd89aed1f0ea300104b0
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-11 17:11:48 +0000

    more_malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,6 +10,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
+	 name = malloc(sizeof(name));
+	 owner = malloc(sizeof(owner));
 	new_dog->name = name;
 	 new_dog->owner = owner;
 	 new_dog->age = age;
