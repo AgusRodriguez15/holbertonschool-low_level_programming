eb0ca9315dce94ab98846d15fb94e7e22d8c054e
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-12 00:52:36 +0000

    more_malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,29 @@
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+int b;
+        while (src[i] != '\0')
+        i++;
+        for (b = 0; b < i; b++)
+        {
+                dest[b] = src[b];
+        }
+        dest[b++] = '\0';
+        return (dest);
+
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
@@ -11,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 	}
 	new_dog->name = name;
-	 new_dog->owner = owner;
+    	new_dog->owner = owner;
 	 new_dog->age = age;
 	 return (new_dog);
 }
