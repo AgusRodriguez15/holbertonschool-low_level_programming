e9900d0859d41bd697fdf37f0179731bfd7fa529
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-12 23:10:25 +0000

    more_malloc

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_name - prints a name as is
+ *@name: name
+ *@f: ddf
  *
  * Return: Nothing.
  */
