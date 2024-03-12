4b6f90bebfbf4babb94cec2b02ee31c446794150
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-12 01:22:01 +0000

    more_malloc

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,13 +1,18 @@
 #include "dog.h"
 #include <stdlib.h>
-
+/**
+ * free_dog - check the code
+ *@d:dd
+ *
+ * Return: Always 0.
+ */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
 }
 
