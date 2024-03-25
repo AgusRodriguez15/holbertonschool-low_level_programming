6a5549afdfdbd85a73b89c9eb99510a728507dd3
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-25 19:09:18 +0000

    singly linked lists

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t cont = 0;
+	size_t cont = 0;
 
-        while (h != NULL)
-        {
-        h = h->next;
-        cont++;
-        }
-        return (cont);
+	while (h != NULL)
+	{
+	h = h->next;
+	cont++;
+	}
+	return (cont);
 }
