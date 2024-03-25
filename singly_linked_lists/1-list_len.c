4ce51f4284248bd4ceafdfc3d45044473964b27b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-25 19:06:30 +0000

    singly linked lists

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - check the code
+ * @h: puntero a la lista
+ *
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+        size_t cont = 0;
+
+        while (h != NULL)
+        {
+        h = h->next;
+        cont++;
+        }
+        return (cont);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -15,4 +15,5 @@ typedef struct list_l
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
