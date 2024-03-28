6a8882a0e1ac380439715a1763cb1622f8105e88
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-28 01:32:30 +0000

    singly_linked_lists third correction plus betty now

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,13 +11,13 @@
 
 int _strlen(const char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	return (i);
 }
 /**
  * list_t *add_node - check the codei
