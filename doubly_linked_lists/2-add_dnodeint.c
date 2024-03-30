42f94168ed704a9c737ee99b20513cc554ae23d2
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-30 20:00:24 +0000

    Doubly_linked_listsTwo

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_dnodeint -  returns the length of a string
+ *@s : char
+ *
+ * Return: Always 0.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+        dlistint_t *addNode = malloc(sizeof(dlistint_t));
+
+        if (addNode == NULL)
+                return (NULL);
+
+        addNode->n = n;
+        addNode->prev = NULL;
+	addNode->next = *head;
+
+        *head = addNode;
+
+        return (addNode);
+	
+}
