dc34b765df3e89a690b5a7d606d7f4860464335a
AgusRodriguez15 <8670@holbertonstudents.com>
2024-03-30 02:23:47 +0000

    singly_linked_lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - check the code
+ *
+ * Return: Always 0.
+ */
+void free_list(list_t *head) 
+{
+    list_t *node = head;
+    list_t *next;
+
+    while (node != NULL) 
+    {
+        next = node->next;
+        free(node);
+        node = next;
+    }
+}
