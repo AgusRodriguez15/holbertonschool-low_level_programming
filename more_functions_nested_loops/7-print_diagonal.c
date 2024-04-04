08b77714d74481b8f6f45ad212e635455ecb8962
AgusRodriguez15 <8670@holbertonstudents.com>
2024-04-04 02:58:18 +0000

    7-print_diagonal.cForPlusScore

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
+/**
+ * print_diagonal - Entry point
+ * @n: in
+ *
+ * Return: Always 0 (Success)
+ */
 void print_diagonal(int n)
 {
-	if (n > 0)
+	int a, b;
+
+	for (a = 0; a < n; a++)
 	{
-		int b;
-		
-		for (b = 0; b < n; b++)
+		for (b = 0; b < a; b++)
 		{
-		
-	_putchar('\\');
-	  _putchar('\n');
-	 _putchar(' ');
+			_putchar(' ');
 		}
-	}
+	_putchar('\\');
 	_putchar('\n');
+}
+	if (n < 0)
+	{
+	_putchar('\n');
+	}
 }
